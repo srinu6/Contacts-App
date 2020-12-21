5746e1c8947ff7108c61e904c4414238727607c9
Srinuvasarao Maripi <srinumaripi.nitsri@gmail.com>
2020-12-21 14:48:26 +0530

    Details Screen Tests

--- __tests__/details-test.js
+++ __tests__/details-test.js
@@ -1,19 +1,20 @@
 /**
  * @jest-environment jsdom
-*/
+ */
 import 'react-native';
 import React from 'react';
 import ContactDetails from '../components/contactDetails';
-import renderer from 'react-test-renderer';
-import { useSelector, useDispatch } from 'react-redux'; 
-import configureStore from 'redux-mock-store'
-import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
-import {Provider} from 'react-redux/src'
-import { mount, shallow } from 'enzyme'
-import {getContact} from '../actions/contactAction'
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import {mount} from 'enzyme';
+// import {Provider} from 'react-redux/src'
+// import {getContact} from '../actions/contactAction'
+// import renderer from 'react-test-renderer';
+// import { useSelector, useDispatch } from 'react-redux';
+// import configureStore from 'redux-mock-store'
 //import Adapter from 'enzyme-adapter-react-16';
 // const mockStore = configureMockStore()
+// const mockStore = configureStore();
 // const store = mockStore({})
 //import storeReal from '../store'
 //import {combineReducers} from '../reducers'
@@ -23,10 +24,14 @@ import {getContact} from '../actions/contactAction'
 //   generateMockReduxStore,
 //   renderWithContext,
 // } from 'testing';
+//   console.log(wrapper.children[1].children[0].children[1].children[1], 'contact details')
+// console.log(wrapper.children[1].children[0].children[1].props)
+//console.log(wrapper.find('View').debug(), 'components inners check')
+
 const mockDispatch = jest.fn();
 jest.mock('react-redux', () => ({
   useSelector: jest.fn(),
-  useDispatch: () => mockDispatch
+  useDispatch: () => mockDispatch,
 }));
 jest.mock('reanimated-bottom-sheet');
 jest.mock('react-native-reanimated', () => {
@@ -52,53 +57,155 @@ jest.mock('react-native-reanimated', () => {
 
 const navigation = {navigate: jest.fn()};
 const route = {
-    params:{
-      contactId: 1,
-    }
-}
-  
+  params: {
+    contactId: 1,
+  },
+};
 
-  
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
-const middlewares = [thunk] 
-const mockStore = configureMockStore(middlewares)
+describe('This will check contacts details screen', () => {
+  const store = mockStore({
+    contactStore: {
+      contact: [
+        {
+          id: 0,
+          firstName: 'Iron',
+          lastName: 'Man',
+          email: 'ironman@hero.com',
+          phone: '9848022335',
+          image: null,
+        },
+        {
+          id: 1,
+          firstName: 'Super',
+          lastName: 'Man',
+          email: 'superman@hero.com',
+          phone: '9876543234',
+          image: null,
+        },
+        {
+          id: 2,
+          firstName: 'Thor',
+          lastName: 'God',
+          email: 'Thor@hero.com',
+          phone: '1234567898',
+          image: null,
+        },
+        {
+          id: 3,
+          firstName: 'Thanos',
+          lastName: 'Conqurer',
+          email: 'Thanos@powerfull.com',
+          phone: '5432145678',
+          image: null,
+        },
+      ],
+    },
+  });
+  const wrapper = mount(
+    <ContactDetails route={route} navigation={navigation} store={store} />,
+  );
+  it('Should check Name of the contact', () => {
+    console.log(
+      wrapper
+        .find('ShowContact')
+        .childAt(0)
+        .childAt(0)
+        .childAt(1)
+        .childAt(0)
+        .childAt(0)
+        .childAt(0)
+        .childAt(0)
+        .childAt(0)
+        .childAt(1)
+        .childAt(0)
+        .text(),
+      'Full Name',
+    );
+    expect(
+      wrapper
+        .find('ShowContact')
+        .childAt(0)
+        .childAt(0)
+        .childAt(1)
+        .childAt(0)
+        .childAt(0)
+        .childAt(0)
+        .childAt(0)
+        .childAt(0)
+        .childAt(1)
+        .childAt(0)
+        .text(),
+    ).toBe('Super Man');
+  });
 
-  describe('This will check contacts details screen',()=>{
-    it('<ContactDetails />', () => {
-      // const mockStore = configureStore();
-      // let wrapper;
-      // let store;
-      const store = mockStore({
-        contactStore: { contact: [
-          {
-            id: 1,
-            firstName: 'Krish',
-            lastName: 'Local',
-            email: 'Krish@hero.com',
-            phone: '9832022335',
-            image: null,
-          },
-        ],
-       }
-      })
-       const wrapper = mount(
-       <Provider store={store}>
-        <ContactDetails
-          route={route}
-          navigation={navigation}          
-        /> 
-       </Provider>
-        )
-      //  console.log(contactReducer(intialState, store.dispatch(getContact(1))), 'store')
-       // console.log(storeReal, 'real store')
-      console.log(wrapper, 'components inners')
-     // console.log(wrapper.children[1].children[0].children[1].children[1], 'contact details')
-        
-     
-       })
-        
-        
-      //expect(addEdit.onUpdateContact().validEmailCheck).toBeTruthy();
-    });
-    
-  
+  it('Should check Name of the contact', () => {
+    console.log(
+      wrapper
+        .find('ShowContact')
+        .childAt(0)
+        .childAt(0)
+        .childAt(1)
+        .childAt(0)
+        .childAt(0)
+        .childAt(0)
+        .childAt(1)
+        .childAt(0)
+        .childAt(1)
+        .childAt(0)
+        .text(),
+      'Phone Number',
+    );
+    expect(
+      wrapper
+        .find('ShowContact')
+        .childAt(0)
+        .childAt(0)
+        .childAt(1)
+        .childAt(0)
+        .childAt(0)
+        .childAt(0)
+        .childAt(1)
+        .childAt(0)
+        .childAt(1)
+        .childAt(0)
+        .text(),
+    ).toBe('9876543234');
+  });
+
+  it('Should check Name of the contact', () => {
+    console.log(
+      wrapper
+        .find('ShowContact')
+        .childAt(0)
+        .childAt(0)
+        .childAt(1)
+        .childAt(0)
+        .childAt(0)
+        .childAt(0)
+        .childAt(2)
+        .childAt(0)
+        .childAt(1)
+        .childAt(0)
+        .text(),
+      'Email ID',
+    );
+    expect(
+      wrapper
+        .find('ShowContact')
+        .childAt(0)
+        .childAt(0)
+        .childAt(1)
+        .childAt(0)
+        .childAt(0)
+        .childAt(0)
+        .childAt(2)
+        .childAt(0)
+        .childAt(1)
+        .childAt(0)
+        .text(),
+    ).toBe('superman@hero.com');
+  });
+});
